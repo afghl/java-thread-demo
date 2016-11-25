11d5c20982cd8352a344177215e9b098abca4588
afghl <airport.0930@gmail.com>
2016-11-25 20:34:35 +0800

    new abstract class

--- src/main/java/threaddemos/Demo1.java
+++ src/main/java/threaddemos/Demo1.java
@@ -14,6 +14,14 @@ class Main {
         Thread th = new Demo1();
         th.start();
 
+        Thread th1 = new Thread() {
+            public void run() {
+                for (int i = 0; i < 10; i++)
+                    System.out.println("running in demo1 thread1!!!!...." + i);
+            }
+        };
+
+        th1.start();
         for (int i = 0; i < 10; i++)
             System.out.println("running in main thread...." + i);
     }
