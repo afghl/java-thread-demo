c2928bd3fea0a93c0fa93dc76ad8d18c3162cfb3
afghl <airport.0930@gmail.com>
2016-11-25 20:26:13 +0800

    demo1

--- src/main/java/threaddemos/Demo1.java
+++ src/main/java/threaddemos/Demo1.java
@@ -1,4 +1,20 @@
 package threaddemos;
 
-public class Demo1 {
+public class Demo1 extends Thread {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++)
+            System.out.println("running in demo1 thread...." + i);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Thread th = new Demo1();
+        th.start();
+
+        for (int i = 0; i < 10; i++)
+            System.out.println("running in main thread...." + i);
+    }
 }
